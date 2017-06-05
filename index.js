8823392d42a0acfdfac805333f03f977cf20206d
Daniel Budick <daniel@budick.eu>
2017-06-05 15:05:21 +0200

    Update index.js
    Added regeneratorRuntime for usage in browser

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ require('babel-core/register');
 require('babel-regenerator-runtime')
 require('babel-polyfill');
 
+/**
+ * This is necessary for Meteor > 1.4
+ */
+const regeneratorRuntime = require('babel-runtime/regenerator');
+if (global.window !== undefined) {
+	if (!Object.keys(global.window).includes('regeneratorRuntime')) {
+		global.window.regeneratorRuntime = regeneratorRuntime
+	}
+}
+if (!Object.keys(global).includes('regeneratorRuntime')) {
+	global.regeneratorRuntime = regeneratorRuntime
+}
+
 module.exports = require('./dist/package').default;
